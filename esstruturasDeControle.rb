8bfc20f634b0c66c7374594f6f98c4466b018473
Fernando Galvão <63373520+FGalvao77@users.noreply.github.com>
2021-03-29 18:32:26 -0300

    conhecendo as estruturas de controle

---
+++ esstruturasDeControle.rb
@@ -0,0 +1,18 @@
+# Estruturas de Controle
+
+# Será abordado:
+
+# Estruturas de Controle
+
+    # Condicional
+        # If/Else/Elsif
+        # Unless
+        # Case
+
+    #Iteração
+        # For
+        # While
+        # Times
+        # Do/While (Loop)
+        
+    # Iteração + Condicional

2a55caf1424044d476a8d9528ea18335a6197464
Fernando Galvão <63373520+FGalvao77@users.noreply.github.com>
2021-03-29 10:36:50 -0300

    conhecendo os métodos/funções para imprimir

--- helloWorld.rb
+++ helloWorld.rb
@@ -1,1 +1,6 @@
-puts 'Hello World!'
+puts 'Hello World!' # a função "puts" imprime algo na tela
+# ou simplificando a função - "p"
+p 'Hi, Fernando!'
+
+# também podemos utilizar a função "print" para imprimir ao algo
+print "Welcome at RUBY"

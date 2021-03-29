96866c8004c1f9e6daad2ae266532ac3c8807714
Fernando Galvão <63373520+FGalvao77@users.noreply.github.com>
2021-03-29 10:40:26 -0300

    entrada e saída de dados

---
+++ entradaSaida.rb
@@ -0,0 +1,9 @@
+# entrada de dado
+print 'Digite seu nome: '
+
+# recebendo uma entrada do teclado
+nome = gets.chomp
+
+# saída utilizando puts
+# use códigos ruby dentro de uma string com #{code}
+puts "Hello, #{nome}!"

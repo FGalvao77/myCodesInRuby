0c3c1c60633ff35ff650254e1147dc95e4fa8ed9
Fernando Galvão <63373520+FGalvao77@users.noreply.github.com>
2021-03-29 13:19:14 -0300

    conhecendo os operadores matemáticos

---
+++ operadoresMatematicos.rb
@@ -0,0 +1,42 @@
+# Operadores Matemáticos
+# para resolver operações matemáticas no ruby 
+# contamos com a seguinte lista de operadores aritméticos:
+
+# + (Adição)
+# – (Subtração)
+# * (Multiplicação)
+# / (Divisão)
+# % (Módulo)
+# ** (Expoente)
+
+# adição
+p -5 + 8
+
+# subtração
+p 9 - 13
+
+# multiplicação
+p 7 * 5
+
+# divisão
+p 21 / 3
+
+# módulo - retorna o resto da divisão
+p 10 % 3
+
+# exponenciação
+p 2 ** 3
+
+# podemos realizar operações matemáticas usando variáveis
+a = 5
+b = 2
+
+print "\nSegue os valores das variáveis: a = #{a} | b = #{b}\n"
+
+print "\n\tOperações matemáticas:\n"
+puts "\t\t1) Soma: #{a + b}"
+puts "\t\t2) Subtração: #{a - b}"
+puts "\t\t3) Multiplicação: #{a * b}"
+puts "\t\t4) Divisão: #{a / b}"
+puts "\t\t5) Módulo da divisao: #{a % b}"
+puts "\t\t6) Exponenciação: #{a ** b}"

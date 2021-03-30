4f64b2a883043a480feb7a366524daacc75a15fa
Fernando Galvão <63373520+FGalvao77@users.noreply.github.com>
2021-03-30 15:03:03 -0300

    Update and rename helloWorld.rb to Aula_01_helloWorld.rb

---
+++ Aula_01_helloWorld.rb
@@ -0,0 +1,22 @@
+# Seu primeiro "Hello World"
+
+# 1. Para fazer seu primeiro programa crie um arquivo dentro da sua máquina ou container ruby "hello.rb"
+	# Obs.: Os arquivos de código ruby possuem a extensão ".rb"
+
+# 2. Abra o arquivo e adicione a seguinte linha.
+
+puts 'Hello, World!' # a função "puts" imprime algo na tela.
+
+# 3. Para executar o programa criado, acesse o Terminal ou SSH Console e rode o comando.
+
+# ruby hello.rb
+
+	# A instrução puts vai imprimir a mensagem 'Hello, World!' toda vez que o programa for excutado.
+
+
+# Podemos também imprimir algo usando as funções "p" e "print".
+	# Como segue os exemplos abaixo:
+	
+p 'Hi, Fernando!'
+
+print "Welcome at RUBY"
--- helloWorld.rb
+++
@@ -1,6 +0,0 @@
-puts 'Hello World!' # a função "puts" imprime algo na tela
-# ou simplificando a função - "p"
-p 'Hi, Fernando!'
-
-# também podemos utilizar a função "print" para imprimir ao algo
-print "Welcome at RUBY"
